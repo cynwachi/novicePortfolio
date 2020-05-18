d2c5a3f0b3b908cbf66cf1712c8be59a553adf68
cynwachi <christinenwachi@gmail.com>
2020-05-18 12:51:50 -0500

    review

---
+++ jsReview2.js
@@ -0,0 +1,21 @@
+
+
+
+
+var name =" Christine, "
+var age = 40
+var age6months = 40.5
+var x;
+
+var i =0;
+while ( i < 3) {
+document.write (name);
+i = i +1 ;
+}
+document.write ("my name is" + name + "and I am " + age + "and in six months I will be" + age6months + "<br>");
+
+var family = ["Christine" , "Marcus" , "Cross" , "Cheyenne"];
+
+var parent = family[0];
+var familySize = family.length;
+
