e113dd89e313a7d1c7e589dc6cfa93170fc47266
cynwachi <christinenwachi@gmail.com>
2020-05-18 15:00:07 -0500

    objects

---
+++ objects.js
@@ -0,0 +1,30 @@
+/**
+ *  Shoes
+ *  SHOE PROPERTIES
+ *  material = "canvas"
+ *  color = "blue"
+ *  type = "running"
+ *  vintage? =  false
+ *  laces? = true
+ *  size = 10.5
+ *  brand  = undefined
+ * 
+ *  SHOE FUNCTIONS:
+ *  tie
+ *  untie
+ *  clean
+ *  wear
+ * 
+ * /
+ var enomaShoe = {
+     material: "Leather",
+     color : "White",
+     type : "casual",
+     vintage : true,
+     laces: false,
+     size: 9,
+     brand: undefined
+    }
+
+    console.log(enomaShoe);
+    console.log(enomaShoe.type);
